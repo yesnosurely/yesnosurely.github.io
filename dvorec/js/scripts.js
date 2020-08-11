4f7b1aa8dd6d8810d38316c8e7c2344ed847bd74
yesnosurely <you@example.com>
2020-08-11 16:49:06 +0300

    dvorec

---
+++ dvorec/js/scripts.js
@@ -0,0 +1,21 @@
+const options = {
+  useEasing : true,
+  useGrouping : true,
+  separator : ' ',
+  decimal : '.',
+  prefix : '',
+  suffix : ''
+};
+const counts = document.querySelectorAll('.block__num');
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	setTimeout(function(){
+		counts.forEach(function(item){
+			const countEnd = item.dataset.countEnd;
+			const count = new CountUp(item, 0, countEnd, 0, 2.5, options);
+			count.start();
+		})
+	},200)
+});
