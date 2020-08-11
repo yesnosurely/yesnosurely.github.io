902d991924bc2ebcafeeac13ed5311e1fe81e5a8
yesnosurely <you@example.com>
2020-08-11 17:45:52 +0300

    upd

--- dvorec/js/scripts.js
+++ dvorec/js/scripts.js
@@ -11,11 +11,9 @@ const counts = document.querySelectorAll('.block__num');
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-	setTimeout(function(){
-		counts.forEach(function(item){
-			const countEnd = item.dataset.countEnd;
-			const count = new CountUp(item, 0, countEnd, 0, 5, options);
-			count.start();
-		})
-	},200)
+	counts.forEach(function(item){
+		const countEnd = item.dataset.countEnd;
+		const count = new CountUp(item, 0, countEnd, 0, 5, options);
+		count.start();
+	})
 });
